39b2beaa1d6e66e5ea481b15cba3d57cc8a1f36b
Luke Preston <lukep@ceh.ac.uk>
2018-03-01 11:58:08 +0000

    deploy

--- src/files/webpack-js/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Thingy = () => <div>THING</div>
-
-export default Thingy
--- src/generate-pages.js
+++ src/generate-pages.js
@@ -3,8 +3,8 @@ const fs = require('fs-extra')
 const path = require('path')
 const mustache = require('mustache')
 
-const build = path.resolve(process.cwd(), 'build')
-const index = path.join(build, 'index.html')
+const dist = path.resolve(process.cwd(), 'dist')
+const index = path.join(dist, 'index.html')
 
 const template = fs.readFileSync(index).toString().replace('<title>Single Page Web App</title>', '<title>{{title}}</title>')
 mustache.parse(template)
@@ -19,9 +19,9 @@ Object.keys(modules).forEach(key => {
       description: link.description || 'A step by step guide to create a modern web/mobile/desktop application'
     })
 
-    const dir = path.join(build, link.pathname)
+    const dir = path.join(dist, link.pathname)
     if (!fs.existsSync(dir)) fs.mkdirSync(dir)
     fs.writeFileSync(path.join(dir, 'index.html'), output)
-    if (link.pathname === '/') fs.writeFileSync(path.join(build, '404.html'))
+    if (link.pathname === '/') fs.writeFileSync(path.join(dist, '404.html'))
   })
 })
