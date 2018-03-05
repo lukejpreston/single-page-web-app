82605cd42741f74d134bc5aecde386eac88042c2
Luke Preston <lukep@ceh.ac.uk>
2018-03-05 11:31:38 +0000

    hidden touch

--- src/chat.js
+++ src/chat.js
@@ -4,7 +4,7 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import {faComments, faTimes} from '@fortawesome/fontawesome-free-solid'
 
 const Chat = ({toggleChat, active}) => <div className={styles.chat()}>
-  <div className={`container ${styles.chatContainer()}`}>
+  <div className={`container is-hidden-touch ${styles.chatContainer()}`}>
     <iframe className={styles.chatTitan(active)}src='https://titanembeds.com/embed/418783769965625344' height='450' frameBorder='0' />
     <button className={`button ${styles.chatButton(active)}`} onClick={toggleChat}>
       <span className='icon'>
